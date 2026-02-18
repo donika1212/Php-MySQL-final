521090558d42af6dcdb20d43adebd227a964b814
donika1212 <donika.pllana@digitalschool.tech>
2026-02-18 19:45:44 +0100

    module2

---
+++ Module_2/string_functions.php
@@ -0,0 +1,12 @@
+<?php
+$the_string = 'Digital School';
+$programming = 'Programming is not cool';
+$another_string = 'Programming';
+
+//echo strlen($the_string);
+//echo str_word_count($the_string);
+//echo str_replace("not", "very", $programming)
+echo strrev($another_string)
+
+//global, local, static
+?>
